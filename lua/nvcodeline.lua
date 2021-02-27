fdf9c83cbf875915f93e22a827256fce7c0e66b8
Syd Amir <hemedani@gmail.com>
2021-02-27 12:22:19 +0330

    🇮 Changes status line flag to ya ali

--- lua/nvcodeline.lua
+++ lua/nvcodeline.lua
@@ -75,7 +75,7 @@ gls.left[2] = {
                           ['!']  = colors.red,
                           t = colors.red}
       vim.api.nvim_command('hi GalaxyViMode guibg='..mode_color[vim.fn.mode()])
-      return '  SydAmir '
+      return '  Ya-ALI '
     end,
     separator = ' ',
     separator_highlight = {colors.yellow,function()
